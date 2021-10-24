14de141836d8adf99e7e39f1a3c12a52e6205218
diegomurillocorrea <diego.murillo@elaniin.com>
2021-10-23 23:26:30 -0600

    Exercise2 initialized

--- Actividad1_DAE_LMP/Program.cs
+++ Actividad1_DAE_LMP/Program.cs
@@ -6,7 +6,7 @@ namespace Actividad1_DAE_LMP
     {
         static void Main(string[] args) {
             Program program = new Program();
-            program.Exercise1();
+            program.Exercise2();
         }
 
         public void Exercise1() {
@@ -46,8 +46,33 @@ namespace Actividad1_DAE_LMP
             Console.WriteLine("\nThanks for choosing Diego's shop!");
         }
 
-        public void Exercise2 () { 
+        public void Exercise2 () {
+            int limit = 0;
+            Console.Write("Enter an amount: ");
+            limit = int.Parse(Console.ReadLine());
 
+            string[] employee_name = new string[limit];
+            string[] employee_dui = new string[limit];
+            string[] employee_nit = new string[limit];
+            double[] employee_salary_per_hour = new double[limit];
+            double[] employee_worked_hours = new double[limit];
+
+            for (int i = 0; i < limit; i++) {
+                Console.WriteLine("Welcome to Diego's Industry!\n");
+                Console.WriteLine($"{i + 1}. Enter the employee name: ");
+                employee_name[i] = Console.ReadLine();
+                Console.WriteLine($"{i + 1}. Enter the employee DUI: ");
+                employee_dui[i] = Console.ReadLine();
+                Console.WriteLine($"{i + 1}. Enter the employee NIT: ");
+                employee_nit[i] = Console.ReadLine();
+                Console.WriteLine($"{i + 1}. Enter the employee salary per hour: ");
+                Console.Write("$");
+                employee_salary_per_hour[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{i + 1}. Enter the employee worked hours: ");
+                Console.Write("$");
+                employee_worked_hours[i] = double.Parse(Console.ReadLine());
+                Console.Clear();
+            }
         }
     }
 }
